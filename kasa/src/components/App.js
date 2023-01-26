bdb11a914c1fd7ecce51dfbfd76877dd3d07f1f5
Nicolas Demangeat <demangeatn@gmail.com>
2023-01-26 11:19:28 +0100

    maj gitignore

--- kasa/src/components/App.js
+++
@@ -1,8 +0,0 @@
-import Router from './Router';
-import React from 'react';
-
-function App() {
-    return <Router />
-}
-
-export default App;
--- kasa/src/components/apropos_banner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function AproposBanner(props) {
-    return (
-        <div className='apropos-banner-contener'></div>
-    )
-}
-
-export default AproposBanner;
